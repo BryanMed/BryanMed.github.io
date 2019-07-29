c54d9b8e5aafb6d3d6fec6770684539cde02ad5c
Bryan Medina <bryan.anidem@gmail.com>
2019-07-28 22:23:58 -0500

    Create 2019-07-28-bordesImagen.py

---
+++ _posts/2019-07-28-bordesImagen.py
@@ -0,0 +1,15 @@
+---
+layout: post
+comments: true
+mathjax: true
+title: 4.3| Detección de bordes en imágenes
+---
+
+## Gradiente de una imagen
+
+Ya hablamos un poquito de la manera en la que las derivadas nos ayudan a encontrar los cambios de intensidad en señales (funciones en 1D). Ahora llegó el momento de 
+
+
+
+
+
